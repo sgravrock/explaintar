c6c07e17f0ca1efff2b02aa19c287e7452414039
Steve Gravrock <sdg@panix.com>
2017-06-27 14:43:27 -0700

    Show the name of the first entry

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::io::{self, Read, Stdin};
+use std::string::FromUtf8Error;
 
 fn main() {
     let mut stdin = io::stdin();
@@ -7,6 +8,11 @@ fn main() {
 
     if header.has_magic() {
         println!("This looks like a valid tar file.");
+
+        match header.name() {
+            Ok(n) => println!("The name of the first entry is {}", n),
+            Err(e) => println!("The first entry's name is invalid: {}", e)
+        };
     } else {
         println!("Bad magic in the first header.");
     }
@@ -50,12 +56,33 @@ impl Header {
         }
     }
 
+    fn name(self: &Header) -> Result<String, FromUtf8Error> {
+        let len = find_zero(&self.block, NAME_LEN).unwrap_or(NAME_LEN);
+        let bytes = self.block[0..len].to_vec();
+        String::from_utf8(bytes)
+    }
+
     fn magic_field(self: &Header) -> &[u8] {
         let offset = NAME_LEN + MODE_LEN + UID_LEN + GID_LEN + SIZE_LEN +
             MTIME_LEN + CHECKSUM_LEN + TYPEFLAG_LEN + LINKNAME_LEN;
         &self.block[offset..(offset + MAGIC_LEN)]
     }
+
+    fn name_field(self: &Header) -> &[u8] {
+        &self.block[0..NAME_LEN]
+    }
 }
+
+fn find_zero(buf: &[u8; 512], maxlen: usize) -> Option<usize> {
+    for i in 0..maxlen {
+        if buf[i] == 0 {
+            return Some(i);
+        }
+    }
+
+    None
+}
+
 
 #[test]
 fn test_has_magic() {
@@ -63,6 +90,18 @@ fn test_has_magic() {
     let bad = block_from_visual("somefile^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@000644 ^@000765 ^@000024 ^@00000000000 13124523641 013414^@ 0^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@nope!");
     assert_eq!(true, Header::from_block(good).has_magic());
     assert_eq!(false, Header::from_block(bad).has_magic());
+}
+
+#[test]
+fn test_name_short() {
+    let block = block_from_visual("somefile^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@x");
+    assert_eq!("somefile", Header::from_block(block).name().unwrap());
+}
+
+#[test]
+fn test_name_exactly_100() {
+    let block = block_from_visual("long________________________________________________________________________________________________x");
+    assert_eq!("long________________________________________________________________________________________________", Header::from_block(block).name().unwrap());
 }
 
 fn block_from_visual(visual: &str) -> [u8;512] {
